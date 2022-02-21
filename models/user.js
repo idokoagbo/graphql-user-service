07d0402704bac68b383d54413aae23a0560742fc
Idoko Agbo <idokojudge@gmail.com>
2022-02-21 10:46:58 -0500

    add user model

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    id: mongoose.Schema.Types.ObjectId,
+    name: String,
+    email: String,
+    phone: String,
+    country: String,
+    password: String,
+    isVerified: {type: Boolean, default: false},
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now}
+});
+
+module.exports = mongoose.model('User', userSchema);
