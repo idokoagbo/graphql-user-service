bbdcc34fef7934c8eb3891af8aae6fd897c33c12
Idoko Agbo <idokojudge@gmail.com>
2022-02-21 16:24:00 -0500

    add sample implementation of email verification

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const jwt = require('express-jwt');
 const startApolloServer = require('./config/apollo')
 const typeDefs = require('./data/schema');
 const resolvers = require('./data/resolvers');
+const { default: axios } = require('axios');
+const { response } = require('express');
 
 mongoose.connect(process.env.MONGO_DB_URI);
 const app = express();
@@ -16,5 +18,38 @@ const auth = jwt({
 });
 
 app.use(auth);
+
+
+/* 
+
+sample implementation of of email verification endpoint 
+this would be done on the frontend facing application of this user service 
+
+*/
+
+
+app.get('/verify', (req, res) => {
+    const email = req.query.email;
+    const token = req.query.token;
+
+    return axios.post(`${process.env.APP_URL}/graphql`, {
+        query: `mutation {
+            verify(email: "${email}", token: "${token}"){
+                id
+            }
+        }`
+    }).then((response) => {
+
+        if (response.data.errors) {
+            return res.status(400).send(response.data.errors[0].message)
+        }
+
+        return res.send(response.data.data);
+    }).catch((error) => {
+        return res.status(500).send({
+            message: error.message
+        });
+    })
+});
 
 startApolloServer(typeDefs, resolvers, app);
